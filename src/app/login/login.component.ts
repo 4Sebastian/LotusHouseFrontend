ddbd25359ca0569a0fb47049f9ef800cca4b5d68
Sebastian Paulis <sebastian4paulis@gmail.com>
2020-11-19 13:36:00 -0500

    moving options into login menu

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { getString, setString } from '@nativescript/core/application-settings';
+import { ObservableArray } from '@nativescript/core/data/observable-array';
 import { IssueService } from '../issue.service';
 
 @Component({
@@ -11,8 +13,20 @@ export class LoginComponent implements OnInit {
   loggedIn:string;
   cnt: number = 0;
 
+  names: ObservableArray<String>;
+  selectedShelter: Number;
+  isChecked: Boolean;
 
-  constructor(private issueService: IssueService) { }
+
+  constructor(private issueService: IssueService) {
+    this.fetchNames();
+    this.selectedShelter = this.getIndex(this.issueService.getShelterName());
+    if(getString("defaultShelter", "false") == "true"){
+      this.isChecked = true;
+    }else{
+      this.isChecked = false;
+    }
+   }
 
   ngOnInit() {
     this.issueService.shareMessage.subscribe(message => this.loggedIn = message);
@@ -54,6 +68,65 @@ export class LoginComponent implements OnInit {
 
   sendResetEmail(){
     console.log("Still need to figure that one out");
+  }
+
+  async fetchNames(){
+    this.names = await this.issueService.getAllNames();
+  }
+
+  dropDownSelectedIndexChanged(i: Number){
+    var string = this.names.getItem(Number(i));
+    if(string != "" && this.matchesOneOfNames(string)){
+      this.issueService.setShelterName(encodeURI(string.toString()));
+    }
+  }
+
+  // giveName(i: Number, checked: Boolean){
+  //   // console.log(Number(i));
+  //   // console.log(i);
+  //   // console.log(this.names.getItem(Number(i)));
+  //   // console.log(encodeURI(this.names.getItem(Number(i)).toString()));
+  //   this.pickedShelter = true;
+  //   try{
+  //     var string = this.names.getItem(Number(i));
+  //     console.log(string);
+  //     if(string != "" && this.matchesOneOfNames(string)){
+  //       this.issueService.setShelterName(encodeURI(string.toString()));
+  //       this.pickedShelter = false;
+  //       console.log("worked");
+  //       setString("defaultShelter", "" + checked);
+  //       console.log("" + checked);
+  //       //transfer to next page;
+  //     }
+      
+  //   }catch(e){
+  //     console.log(e);
+  //   }
+    
+    
+  // }
+
+  matchesOneOfNames(string: String){
+    for(var i = 0; i < this.names.length; i++){
+      if(string == this.names.getItem(i)){
+        return true;
+      }
+    }
+    return false;
+  }
+
+  getIndex(name: String){
+    const temp = decodeURI(name.toString());
+    for(var i = 0; i < this.names.length; i++){
+      if(temp == this.names.getItem(i)){
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  onCheckedChanged(state: Boolean){
+    setString("defaultShelter", "" + state);
   }
 
 }
